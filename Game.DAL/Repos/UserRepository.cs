dafae6bf31bc42451705538ea311c286ec09eaa9
Nuhin <153355458+ShanjidulNuhin@users.noreply.github.com>
2026-05-18 23:11:08 +0600

    add user repo

---
+++ Game.DAL/Repos/UserRepository.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Game.DAL.EF;
+using Game.DAL.EF.Tables;
+
+namespace Game.DAL.Repos
+{
+    public class UserRepository
+    {
+        private readonly GameSpdbContext db;
+
+        public UserRepository(GameSpdbContext db)
+        {
+            this.db = db;
+        }
+        public User? GetByEmail(string email)
+        {
+            return db.Users.FirstOrDefault(u => u.Email == email);
+        }
+        public User? GetByToken(string token)
+        {
+            return db.Users.FirstOrDefault(u => u.token == token);
+        }
+        public bool Create(User u)
+        {
+            db.Users.Add(u);
+            return db.SaveChanges() > 0;
+        }
+        public bool Update(User u) {
+            db.Users.Update(u);
+           return db.SaveChanges()>0;
+        }
+    }
+}
