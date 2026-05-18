24feffbe274b75bc1b7cb2bf0b1913ff2100ccd3
Nuhin <153355458+ShanjidulNuhin@users.noreply.github.com>
2026-05-18 22:28:00 +0600

    adding not added

--- App/obj/Debug/net10.0/App.AssemblyInfo.cs
+++ App/obj/Debug/net10.0/App.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("App")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9783c37a21b22b9ef27ab2cfbf62dffe8025222f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+95b99228b483bc55024755540183622cde7319da")]
 [assembly: System.Reflection.AssemblyProductAttribute("App")]
 [assembly: System.Reflection.AssemblyTitleAttribute("App")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
