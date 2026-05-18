9783c37a21b22b9ef27ab2cfbf62dffe8025222f
Nuhin <153355458+ShanjidulNuhin@users.noreply.github.com>
2026-05-18 14:35:59 +0600

    update DTOs

--- Game.BLL/DTOs/GameCreateDTO.cs
+++ Game.BLL/DTOs/GameCreateDTO.cs
@@ -8,6 +8,7 @@ namespace BLL.DTOs
     {
         public string Title { get; set; } = "";
         public string Genre { get; set; } = "";
+        public string? Cover { get; set; }
         public string? Description { get; set; }
         public decimal Price { get; set; }
     }
--- Game.BLL/DTOs/GameDTO.cs
+++ Game.BLL/DTOs/GameDTO.cs
@@ -9,7 +9,8 @@ namespace Game.BLL.DTOs
         public int Id { get; set; }
         public string Title { get; set; }
         public string Genre { get; set; }
-        public string Description { get; set; }
+        public string? Cover { get; set; }
+        public string? Description { get; set; }
         public decimal Price { get; set; }
 
     }
