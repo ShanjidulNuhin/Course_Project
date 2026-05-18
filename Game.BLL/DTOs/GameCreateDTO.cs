b5ca3e5894bcbdea92ee59bbdff0d704cfde553c
Nuhin <153355458+ShanjidulNuhin@users.noreply.github.com>
2026-05-18 22:22:18 +0600

    fix

--- Game.BLL/DTOs/GameCreateDTO.cs
+++ Game.BLL/DTOs/GameCreateDTO.cs
@@ -8,7 +8,7 @@ namespace BLL.DTOs
     {
         public string Title { get; set; } = "";
         public string Genre { get; set; } = "";
-        public string? Cover { get; set; }
+        public byte[] Cover { get; set; }
         public string? Description { get; set; }
         public decimal Price { get; set; }
     }
